b6cb70cb64a0c51a340e5cdd5b5042b55e25bee0
khoahd <hoangdangkhoa0797@gmail.com>
2022-12-06 11:41:22 +0700

    k

--- kubernetes/metallb.sh
+++ kubernetes/metallb.sh
@@ -2,5 +2,6 @@
 kubectl edit configmap -n kube-system kube-proxy
 
 kubectl apply -f https://raw.githubusercontent.com/metallb/metallb/v0.13.7/config/manifests/metallb-native.yaml
-
+nano metallb-config.yaml
+kubectl get po -n metallb-system --watch
 kubectl apply -f metallb-config.yaml
