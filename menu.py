415b19285b8eb660e0947192d425ed0f3111a51a
khoahd <hoangdangkhoa0797@gmail.com>
2022-12-06 17:26:44 +0700

    k

--- menu.py
+++ menu.py
@@ -6,7 +6,7 @@ from simple_term_menu import TerminalMenu
 
 def main():
     main_menu_title = "  Main Menu.\n  Press Q or Esc to quit. \n"
-    main_menu_items = ["[1] Install ansible", "[2] Edit hosts file", "[3] Edit variables file", "[4] Install swarm cluster (Production)", "[5] Install swarm cluster only one node (Test)", "Quit"]
+    main_menu_items = ["[1] Install ansible", "[2] Edit hosts file", "[3] Edit variables file", "[4] Install swarm cluster (Production)", "[5] Deploy Kubernetes", "Quit"]
     main_menu_cursor = ">> "
     main_menu_cursor_style = ("fg_blue", "bold")
     main_menu_style = ("bg_red", "fg_yellow")
@@ -23,7 +23,7 @@ def main():
     )
 
     install_swarm_title = "  Install cluster.\n  Press Q or Esc to back to main menu. \n"
-    install_swarm_items = ["[1] Install multi-manage multi-worker cluster *", "[2] Install manager reachable", "[3] Install worker", "[4] Iake token", "Back to Main Menu"]
+    install_swarm_items = ["[1] Install multi-manage multi-worker cluster *", "[2] Install manager reachable", "[3] Install worker","[4] Install swarm cluster only one node (Test)", "Back to Main Menu"]
     install_swarm_back = False
     install_swarm = TerminalMenu(
         install_swarm_items,
@@ -44,7 +44,7 @@ def main():
             subprocess.run('ssh-keygen -b 2048 -t rsa -f /root/.ssh/id_rsa -q -N "" 0>&-', shell=True)
         if main_sel == 1:
             print("Cập nhật file hosts")
-            time.sleep(2)
+            time.sleep(1)
             subprocess.run("nano hosts; cp -f ./hosts /etc/ansible/hosts", shell=True)
             print("Copy keygen ssh: write Yes and import Password")
             time.sleep(2)
@@ -62,21 +62,22 @@ def main():
                     subprocess.run("ansible-playbook playbook/umanager.yml; ansible-playbook playbook/managerjoin.yml; ansible-playbook playbook/workerjoin.yml; ansible-playbook playbook/replaceManagerToWorker.yml; ansible-playbook playbook/portainerinstall.yml; ansible-playbook playbook/traefikinstall.yml; ansible-playbook playbook/infrainstall.yml; ansible-playbook playbook/Log4jSecurityVulnerability.yml", shell=True)
                 elif edit_sel == 1:
                     print("Install manager reachable")
-                    subprocess.run("ansible-playbook playbook/managerjoin.yml", shell=True)
+                    subprocess.run("ansible-playbook playbook/umanager.yml --tags token; ansible-playbook playbook/managerjoin.yml", shell=True)
                 elif edit_sel == 2:
                     print("install worker")
-                    subprocess.run("ansible-playbook playbook/workerjoin.yml", shell=True)
+                    subprocess.run("ansible-playbook playbook/umanager.yml --tags token; ansible-playbook playbook/workerjoin.yml", shell=True)
                 elif edit_sel == 3:
-                    print("take token")
-                    subprocess.run("ansible-playbook playbook/umanager.yml --tags token", shell=True)                    
+                    print("install swarm cluster only one node")
+                    subprocess.run("ansible-playbook playbook/umanager.yml; ansible-playbook playbook/portainerinstall.yml --skip-tags git; ansible-playbook playbook/traefikinstall.yml --skip-tags git; ansible-playbook playbook/infrainstall.yml --skip-tags git", shell=True)
+                    time.sleep(3)
                 elif edit_sel == 4 or edit_sel == None:
                     install_swarm_back = True
                     print("Back Selected")
             install_swarm_back = False
         elif main_sel == 4:
-            print("install swarm cluster only one node")
-            subprocess.run("ansible-playbook playbook/umanager.yml; ansible-playbook playbook/portainerinstall.yml --skip-tags git; ansible-playbook playbook/traefikinstall.yml --skip-tags git; ansible-playbook playbook/infrainstall.yml --skip-tags git", shell=True)
-            time.sleep(5)
+            print("Deploy Kubernetes")
+            subprocess.run("ansible-playbook kubernetes/dependent-components.yml; ansible-playbook kubernetes/master.yml; ansible-playbook kubernetes/worker.yml", shell=True)
+            time.sleep(3)
         elif main_sel == 5 or main_sel == None:
             main_menu_exit = True
             print("Quit Selected")
