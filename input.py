2151025b6cae47619bab1c84adfcf7a5c4865716
khoahd <hoangdangkhoa0797@gmail.com>
2022-12-08 14:24:18 +0700

    k

---
+++ input.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+import time
+import os
+import subprocess
+
+def main():
+    firstName = input('Enter your first name: ')
+    print('Hello',firstName)
+
+if __name__ == "__main__":
+    main()
