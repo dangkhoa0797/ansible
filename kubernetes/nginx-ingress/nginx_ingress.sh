a2339e0f8de5f882dcebb0b6e304abd0aa7c2f3d
khoahd <hoangdangkhoa0797@gmail.com>
2022-12-15 16:32:15 +0700

    k

---
+++ kubernetes/nginx-ingress/nginx_ingress.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+helm upgrade --install ingress-nginx ingress-nginx   --repo https://kubernetes.github.io/ingress-nginx   --namespace ingress-nginx --create-namespace --values ingress-nginx-values.yaml
