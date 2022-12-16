2b940b5e371937cd4f6e767d5cd693740cf3ad79
khoahd <hoangdangkhoa0797@gmail.com>
2022-12-16 10:05:13 +0700

    k

--- kubernetes/nginx-ingress/nginx_ingress.sh
+++ kubernetes/nginx-ingress/nginx_ingress.sh
@@ -2,4 +2,4 @@
 helm upgrade --install ingress-nginx ingress-nginx   --repo https://kubernetes.github.io/ingress-nginx   --namespace ingress-nginx --create-namespace --values ingress-nginx-values.yaml
 
 #https://cert-manager.io/docs/installation/
-#kubectl apply -f https://github.com/cert-manager/cert-manager/releases/download/v1.10.1/cert-manager.yaml
+kubectl apply -f https://github.com/cert-manager/cert-manager/releases/download/v1.10.1/cert-manager.yaml
