a8b4389c19d62d3ee84f3ea9537789e9e844bc41
khoahd <hoangdangkhoa0797@gmail.com>
2022-12-23 13:38:38 +0700

    k

---
+++ kubernetes/traefik/traefik.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+helm repo add traefik https://helm.traefik.io/traefik
+helm repo update
+helm show values traefik --repo https://helm.traefik.io/traefik > values.yaml
+#helm install --values=./values.yml traefik --set experimental.kubernetesGateway.enabled=true traefik/traefik --namespace traefik --create-namespace
+
+#helm upgrade traefik -f values.yaml traefik/traefik -n traefik
