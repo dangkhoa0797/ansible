28dac851df4387572c1ee9e523eaf88811e26f81
khoahd <hoangdangkhoa0797@gmail.com>
2023-01-04 16:57:32 +0700

    k

--- k8s/k8smaster.sh
+++ k8s/k8smaster.sh
@@ -59,13 +59,8 @@ kubeadm init --control-plane-endpoint=k8smaster.ubuntu.net
 #kubeadm init --pod-network-cidr=10.244.0.0/16
 
 #kubeadm init --config kubeadm-config.yaml
-mkdir -p $HOME/.kube
-sudo cp -i /etc/kubernetes/admin.conf $HOME/.kube/config
-sudo chown $(id -u):$(id -g) $HOME/.kube/config
+kubectl apply -f https://raw.githubusercontent.com/coreos/flannel/master/Documentation/kube-flannel.yml
 
-#kubectl create -f https://raw.githubusercontent.com/projectcalico/calico/v3.24.5/manifests/tigera-operator.yaml
-#curl https://raw.githubusercontent.com/projectcalico/calico/v3.24.5/manifests/custom-resources.yaml -O
-#kubectl create -f custom-resources.yaml
 
 curl https://projectcalico.docs.tigera.io/manifests/calico.yaml -O
 kubectl apply -f calico.yaml
