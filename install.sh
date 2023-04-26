17426cf43717fe7bda81ae5c78a217836a175553
Hoang Dang Khoa <khoahoang@cqtdt.asia>
2023-04-26 17:35:36 +0700

    kd

--- install.sh
+++ install.sh
@@ -3,6 +3,4 @@ yum -y install ansible
 ssh-keygen -b 2048 -t rsa -f /root/.ssh/id_rsa -q -N "" 0>&-
 ssh-copy-id -i ~/.ssh/id_rsa.pub root@10.1.20.13
 # ansible-playbook example.yaml
-
-sudo yum -y install epel-release python-pip
-pip install pick
+# devops="/root/menu.sh"
