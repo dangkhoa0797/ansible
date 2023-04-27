fd00a17310e06fdc9d0eaa0933c9ed0e1d992aba
Hoang Dang Khoa <khoahoang@cqtdt.asia>
2023-04-27 17:28:17 +0700

    kh

--- grafana/setup.sh
+++ grafana/setup.sh
@@ -5,16 +5,16 @@ helm repo add grafana https://grafana.github.io/helm-charts
 helm repo update
 
    
-   helm upgrade -i loki grafana/loki-stack -n monitor --create-namespace \
-     --set grafana.enabled=true,grafana.adminPassword=Tdt@123456 \
-     --set prometheus.enabled=true,prometheus.alertmanager.persistentVolume.enabled=false,prometheus.server.persistentVolume.enabled=false \
-     --set alertmanager.enabled=true \
-     --set loki.persistence.enabled=true,loki.persistence.size=10000000Gi,loki.persistence.storageClassName=nfs-client \
-     --set promtail.enabled=true,promtail.config.lokiAddress=http://loki:3100/api/prom/push
-   
-   kubectl expose service loki-grafana --type=LoadBalancer --name=grafana-svc --namespace=monitor
-   
-   kubectl get svc grafana-svc -n loki-stack -o jsonpath='{.status.loadBalancer.ingress[0].hostname}'
-   kubectl port-forward service/loki-grafana 3000:80 --address='0.0.0.0' -n monitor
+helm upgrade -i loki grafana/loki-stack -n monitor --create-namespace \
+  --set grafana.enabled=true,grafana.adminPassword=Tdt@123456 \
+  #--set grafana.service.type=NodePort \
+  #--set grafana.service.nodePort=30100 \
+  --set prometheus.enabled=true,prometheus.alertmanager.persistentVolume.enabled=false,prometheus.server.persistentVolume.enabled=false \
+  --set alertmanager.enabled=true \
+  --set loki.persistence.enabled=true,loki.persistence.size=10000000Gi,loki.persistence.storageClassName=nfs-client \
+  --set promtail.enabled=true,promtail.config.lokiAddress=http://loki:3100/api/prom/push
 
-   --set alertmanager.enabled=true
+kubectl expose service loki-grafana --type=LoadBalancer --name=grafana-svc --namespace=monitor
+
+kubectl get svc grafana-svc -n loki-stack -o jsonpath='{.status.loadBalancer.ingress[0].hostname}'
+kubectl port-forward service/loki-grafana 3000:80 --address='0.0.0.0' -n monitor
