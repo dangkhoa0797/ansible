01ef18ce4b420eef749c6025199734e66f581ad2
Hoang Dang Khoa <khoahoang@cqtdt.asia>
2023-04-28 15:11:41 +0700

    kahf

--- Docker/Docker-rigistry/setup.sh
+++ Docker/Docker-rigistry/setup.sh
@@ -4,6 +4,7 @@ services:
   registry:
     image: registry:2
     volumes:
+      - ./config/config.yml:/etc/docker/registry/config.yml
       - ./data:/var/lib/registry:rw
     ports:
       - "5000:5000"
@@ -18,6 +19,7 @@ services:
       - REGISTRY_TITLE=My Private Docker Registry
       - REGISTRY_URL=http://registry:5000
       - DELETE_IMAGES=true
+      - THEME=auto
     depends_on:
       - registry
     networks:
